00ad919822c5d542db2419aac9ce098de60235fc
tkxwaweru <88835284+tkxwaweru@users.noreply.github.com>
2022-09-06 23:30:29 +0300

    Update index.php
    Trevor's portion of the index page

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>KaribuKenya.com</title>
+    <link rel="stylesheet" href="index-style.css">
+  </head>
+  <body>
+    <div class="header-container">
+      <div class="header-contents">
+        <img class="logo" src="Index-Images\Logo.png" alt="Karibu Kenya logo">
+        <a class="links" href=""> Support </a> 
+        <a class="links" href=""> Destinations </a>
+        <a class="links" href=""> Sign up </a>
+        <a class="links" href=""> Log in </a>
+        <a class="links" href=""> Home </a>
+      </div>
+      <div class="description">
+        <h1>Experience the beauty and spectacles of Kenya.</h1>
+        <h2>Leisure? Adventure? We've got you covered.</h2>
+        <a class="book-button" href="">BOOK NOW</a>
+      </div>
+    </div>
+    <div class="benefits-list">
+      <div class="benefit">
+        <img class="benefit-icon" src="Index-Images\destination-icon.png" alt="Destinaton icon">
+        <h3>Spectacular Destinations</h3>
+        <p>
+          From the sandy beached to the diverse wildlife, Kenya's attractions are sure to amaze.
+        </p>
+      </div>
+      <div class="benefit">
+        <img class="benefit-icon" src="Index-Images\discount-icon.png" alt="Destinaton icon">
+        <h3>Offers & Discounts</h3>
+        <p>
+          We offer various seasonal offers and discounts to ease travel costs.
+        </p>
+      </div>
+      <div class="benefit">
+        <img class="benefit-icon" src="Index-Images\support-icon.png" alt="Destinaton icon">
+        <h3>Amazing Customer Support</h3>
+        <p>
+          Our highly trained team of travel agents are always ready to assist you with your booking related issues. 
+        </p>
+      </div>
+      <div class="benefit">
+        <img class="benefit-icon" src="Index-Images\booking-logo.png" alt="Destinaton icon">
+        <h3>Easy bookings</h3>
+        <p>
+          All you need to do is tell us where you want to go and stay and we'll handle the rest.
+        </p>
+      </div>
+    </div>
