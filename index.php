4f51e80f1fcd6356bf613b3a6aa72063dfac9240
tkxwaweru <88835284+tkxwaweru@users.noreply.github.com>
2022-09-07 10:49:34 +0300

    Update index.php
    Combined homepage from Trevor & Patience

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>KaribuKenya.com</title>
+    <title></title>
     <link rel="stylesheet" href="index-style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
   </head>
   <body>
     <div class="header-container">
@@ -17,6 +19,9 @@
       <div class="description">
         <h1>Experience the beauty and spectacles of Kenya.</h1>
         <h2>Leisure? Adventure? We've got you covered.</h2>
+        <br>
+        <br>
+        <br>
         <a class="book-button" href="">BOOK NOW</a>
       </div>
     </div>
@@ -50,3 +55,148 @@
         </p>
       </div>
     </div>
+  </p><div class="testimonials">
+    <div class="inner">
+      <h1>Testimonials</h1>
+      <div class="border"></div>
+
+      <div class="row">
+        <div class="col">
+          <div class="testimonial">
+            <img src="Index-Images\p4.jpg" alt="">
+            <div class="name">Julius Kavita</div>
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+
+            <p>
+              Travelling to a new place could be difficult but with Karibu Kenya my family and I 
+              have the bes
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="testimonial">
+            <img src="Index-Images\p2.png" alt="">
+            <div class="name">Rose Daniel</div>
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+
+            <p>
+            Works like a charm. I love the ease of the website and would highly
+            recommend using their services for any safari plans you have to make to kenya.
+
+            </p>
+          </div>
+        </div>
+
+        <div class="col">
+          <div class="testimonial">
+            <img src="Index-Images\p3.png" alt="">
+            <div class="name">Michelle Lulu</div>
+            <div class="stars">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+
+            <p>
+              Karibu Kenya was amazing in helping us book our first kenyan beach experience.
+              <br>They are very accomodating,Thankyou very much hope to work wit you again.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="destinations">
+    <div class=places> 
+      <h4>Where would you like to experience Kenya?</h4>
+    </div>
+    <div class="images">
+      <img src="Index-Images\p5.jfif" alt=""><p>diani costal beach</p>
+    </div>
+
+     <div class="images"> 
+      <img src="Index-Images\p6.jfif" alt=""><P>Maasai mara</P>
+     </div>
+
+     <div class="images"> <img src="Index-Images\p7.jfif" alt=""><p>mount kenya</p>
+     </div>
+
+     <div class="images"><img src="Index-Images\p8.jfif" alt=""><p>nature walks</p>
+     </div>
+
+     <div class="images"> <img src="Index-Images\p9.jfif" alt=""><p>hot springs in bogoria</p>
+     </div>
+    
+
+    
+  </div>
+
+
+  <footer class="footer">
+  <div class="container">
+    <div class="row"> 
+
+      <div class="footer-col">
+        <h4>organization</h4>
+        <ul>
+          <li><a href="">about us</a></li>
+          <li><a href="">our services</a></li>
+          <li><a href="">privacy policy</a></li>
+          <li><a href="">contacts</a></li>
+        </ul>
+      </div> 
+      <div class="footer-col">
+        <h4>Get help</h4>
+        <ul>
+          <li><a href="">FAQ</a></li>
+          <li><a href="">contact info</a></li>
+          <li><a href="">booking info</a></li>
+          <li><a href="">payments</a></li>
+        </ul>
+      </div> 
+
+      <div class="footer-col">
+        <h4>online booking</h4>
+        <ul>
+          <li><a href="">safari tours</a></li>
+          <li><a href="">beach enjoyment</a></li>
+          <li><a href="">mountain climbing</a></li>
+          <li><a href="">water sports</a></li>
+        </ul>
+      </div> 
+
+      <div class="footer-col">
+        <h4>follow us</h4>
+        <div class="social-links">
+        <a href="" ><i class="fab fa-facebook-f"></i></a>
+        <a href="" ><i class="fab fa-twitter"></i></a>
+        <a href="" ><i class="fab fa-instagram"></i></a>
+        <a href="" ><i class="fab fa-linkedin-in"></i></a>
+      </div>
+        
+        <ul>
+          <li><a href=""></a></li>
+        </ul>
+
+      </div>
+    </div>
+  </div> 
+ </footer>
+</body>
+</html>
