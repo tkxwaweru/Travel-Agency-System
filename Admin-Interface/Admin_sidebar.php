dc353150229c24a513d284b9689be8528da76c3e
Bramwelktum <74041018+Bramwelktum@users.noreply.github.com>
2022-09-28 09:07:14 +0300

    Add files via upload

---
+++ Admin-Interface/Admin_sidebar.php
@@ -0,0 +1,45 @@
+        <header class="header">
+            <a href="">Admin Dashboard</a>
+            <div class="logout">
+                <a href="" class="btn btn-info">Logout</a>
+            </div>
+        </header>
+        <aside>
+            <ul>
+                <li>
+                    <a href="">View Admin Details</a>
+                </li>
+
+                <li>
+                    <a href="add_agent.php">Add Agent</a>
+                </li>
+
+                <li>
+                    <a href="">View Agent</a>
+                </li>
+
+                <li>
+                    <a href="">Client-Agent Assignment</a>
+                </li>
+
+                <li>
+                    <a href="add_client.php">Add Client</a>
+                </li>
+
+                <li>
+                    <a href="">View Client</a>
+                </li>
+                
+                <li>
+                    <a href="add_destination.php">Add Destination</a>
+                </li>
+
+                <li>
+                    <a href="">View Destination</a>
+                </li>
+
+                <li>
+                    <a href="add_activity.php">Add Activity</a>
+                </li>
+            </ul>
+        </aside>
