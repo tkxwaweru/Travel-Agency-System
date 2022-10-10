4795591ca03d002cc88ad0ae9b5d5a64ac2302c7
tkxwaweru <88835284+tkxwaweru@users.noreply.github.com>
2022-10-10 19:28:29 +0300

    Add files via upload

---
+++ Client-Interfaces/client-dashboard-home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Dashboard.home</title>
+  <link rel="stylesheet" href="client-dashboard-home.css" />
+  <!-- Font Awesome Cdn Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+  <header class="header">
+    <div class="title">
+      <span>Dashboard</span>
+    </div>
+    <div class="header-icons">
+      <div class="account">
+        <h4>[user first name]</h4>
+      </div>
+    </div>
+  </header>
+
+  <div class="container">
+    <nav>
+      <div class="side_navbar">
+        <a class="active" href="client-dashboard-home.php">Home</a>
+        <a href="explore-attractions.php">Explore attractions</a>
+        <a href="booking-request.php">Booking requests</a>
+        <a href="booking-history.php">Booking history</a>
+        <a href="client-invoices.php">Invoices</a>
+        <a href="client-payments.php">Payment</a>
+        <a class="log-out-button" href="../Index/index.php">Log out</a>
+      </div>
+    </nav>
+
+    <div class="main-body">
+      <h2>Home</h2>
+      <div class="promo_card">
+        <h1>Welcome [user first name].</h1>
+        <span>Use the sidebar to access a variety of options.</span>
+        <a href="booking-request-test.php">BOOK NOW</a>
+      </div>
+    </div>
+    </div>
+  </div>
+</body>
+</html>
