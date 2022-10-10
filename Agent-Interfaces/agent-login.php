9dfeb826f4336b93e7366aa3f073081a1924d3cc
tkxwaweru <88835284+tkxwaweru@users.noreply.github.com>
2022-10-10 18:30:43 +0300

    Add files via upload

---
+++ Agent-Interfaces/agent-login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Agent Login</title>
+		<link rel="stylesheet" href="agent-login.css">
+	</head>
+	<body>
+		<div class="registration-background">
+      <div class="background-image">
+        <img src="../Index/Images/passport.jpg" alt="background image">
+      </div>
+      <div class="registration-form">
+        <div class="form-content">
+          <h2>Login</h2>
+          <form action="../Processes/login-process.php" method="post"> 
+            <div class="input">
+              <span>Email</span>
+              <input type="text" id="email" name="client_email" required> 
+            </div>
+            <div class="input">
+              <span>Password</span>
+              <input type="Password" id="password" name="password" required>
+            </div>
+            <div class="remember">
+              <label><input type="checkbox">Remember me</label>
+            </div>
+            <div class="input">
+              <button type="submit" class="register-button">Log in</button>
+            </div>
+            <div class="input">
+              <p>Administrator? <a href="../Admin-Interface/admin-login.php">Log in here</a></p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>  
+	</body>
+</html>
