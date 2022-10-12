51715d980680ffe78cd7a5e83c8bd082e143df28
patience-kamanthe <101176716+patience-kamanthe@users.noreply.github.com>
2022-10-12 10:11:43 +0300

    Add files via upload

---
+++ Client-Interfaces/attractions.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Attractions page</title>
+        <link rel="stylesheet" href="attractions.css">
+    </head>
+
+    <body>
+<div class="main-container ">
+
+<div class="container">
+  <img src="images/amboseli.jpg" alt="amboseli" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>AMBOSELI</h1>
+         The park is famous for being the best place in the world<br>
+         to get close to free-ranging elephants. Other attractions of<br>
+          the park include opportunities to meet Maasai and visit a Maasai village.<br>
+         </div>
+  </div>
+ 
+  
+
+</div>
+
+<div class="container">
+  <img src="Images\diani-beach.jpg" alt="diani beach" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>DIANI BEACH</h1>
+        Diani has a population of around 100,000 inhabitants and is famous for its white sand beaches,<br>
+         blue ocean and (kite)surfing. The beach is popular for relaxing resorts which are popular with honeymooners,
+          backpackers and families on holiday
+        </div>
+  </div>
+ 
+  
+
+</div>
+
+<div class="container">
+  <img src="Images\fort-jesus.jpg" alt="fort Jesus" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>HISTORICAL SITES</h1>
+        The Fort, built by the Portuguese to the designs of Giovanni to protect the port of Mombasa,<br>
+         is one of the most outstanding and well preserved examples of<br>
+          16th Portuguese military fortification and a landmark in the history of this type of construction.
+        </div>
+  </div>
+
+</div>
+
+<div class="container">
+  <img src="Images\hotel-pools.jpg" alt="hotel pool" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>RESORTS</h1>
+        Our world class resorts provide you with the best hospitality services<br>
+        ensuring you get to enjoy our kenyan meals and have the best time relaxing<br>
+        if you are looking for a good time of peace then these are the places to visit.
+        </div>
+  </div>
+
+</div>
+
+<div class="container">
+  <img src="Images\maasai-mara-elephants.jpg" alt="elephants" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>ELEPHANTS</h1>
+        They're the world's largest land animal. The African Savanna (Bush) elephant <br>
+        is the world's largest land animal with adult males, or bull elephants,<br>
+         standing up to 3m high and weighing up to 6,000kg on average.
+        </div>
+  </div>
+
+</div>
+
+<div class="container">
+  <img src="Images\maasai-mara-balloons.jpg" alt="hot air baloon" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>HOT AIR BALLOONS</h1>
+        For the perfect view of the kenyan Savanah, the migrating animals and <br>
+        the beautiful sunset and sunrise, these baloons provide you with the best view<br>
+        at very affordable prices.
+        </div>
+  </div>
+
+</div>
+
+<div class="container">
+  <img src="Images\mountain-bongo-laikipia.jpg" alt="mountain bongo" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>DIVERSE WILDLIFE</h1>
+        A home to many different species, each in their own environments<br>
+        form the rainforest to the dry grasslands of the Kenyan savannah<br>
+        and many more to be expored by you.
+        </div>
+  </div>
+
+</div>
+
+
+<div class="container">
+  <img src="Images\nairobi.jpg" alt="nairobi city" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>NAIROBI CITY</h1>
+        Now one of Africa's most influential cities, Nairobi is a vital commercial<br>
+         and financial regional hub, home to the regional headquarters of various major <br>
+         international companies and organizations and the only capital city housing a national park.
+
+
+        </div>
+  </div>
+
+</div>
+
+
+<div class="container">
+  <img src="Images\mara-lion.jpg" alt="the big 5" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>THE BIG 5</h1>
+        The term “Big Five” originally referred to the difficulty in hunting the<br>
+         lion, leopard, rhino, elephant and African buffalo. These five large African mammal <br>
+         species were known to be dangerous and it was considered a feat by trophy hunters to bring them home.
+
+
+        </div>
+  </div>
+
+</div>
+
+
+<div class="container">
+  <img src="Images\maasai-tourist.jpg" alt="Maasai tribe" class="image">
+  <div class="overlay">
+    <div class="text">
+        <h1>RICH CULTURE</h1>
+        Maasai culture is centred around the belief that God (called Engai) created cattle <br>
+        especially for them, and they are the custodians of all the world's cattle. <br>
+        For Maasai, life revolves around amassing and grazing large herds of cows .
+
+
+        </div>
+  </div>
+
+</div>
+
+
+
+</div>
+
+    </body>
+</html>
