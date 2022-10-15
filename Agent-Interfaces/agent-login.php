41de7ad22ec449fad942b08e9ca04dd4471a38a7
myles4321 <88835357+myles4321@users.noreply.github.com>
2022-10-15 12:07:01 +0300

    Update agent-login.php

--- Agent-Interfaces/agent-login.php
+++ Agent-Interfaces/agent-login.php
@@ -14,20 +14,20 @@
       <div class="registration-form">
         <div class="form-content">
           <h2>Login</h2>
-          <form action="../Processes/login-process.php" method="post"> 
+          <form action="../Processes/agent-login-process.php" method="post"> 
             <div class="input">
               <span>Email</span>
-              <input type="text" id="email" name="client_email" required> 
+              <input type="text" id="email" name="agent_email" required> 
             </div>
             <div class="input">
               <span>Password</span>
-              <input type="Password" id="password" name="password" required>
+              <input type="Password" id="password" name="agent_password" required>
             </div>
             <div class="remember">
               <label><input type="checkbox">Remember me</label>
             </div>
             <div class="input">
-              <button type="submit" class="register-button">Log in</button>
+              <button type="submit" name="login" class="register-button">Log in</button>
             </div>
             <div class="input">
               <p>Administrator? <a href="../Admin-Interface/admin-login.php">Log in here</a></p>
