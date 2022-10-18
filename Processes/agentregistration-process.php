828a6335d151541a3a56fccf8bc0a7d376096512
Ikirema <88835650+Ikirema@users.noreply.github.com>
2022-10-18 19:52:13 +0300

    Update agentregistration-process.php

--- Processes/agentregistration-process.php
+++ Processes/agentregistration-process.php
@@ -8,7 +8,7 @@ $email = $_POST['agent_email'];
 $password = $_POST['agent_password'];
 
 
-$sql= "INSERT INTO `clients`(`client_fname`, `client_lname`, `phone_number`, `client_email`, `password`) VALUES ('$fname', '$lname','$phonenumber', '$email', '$password')";
+$sql= "INSERT INTO `agents`(`agent_fname`, `agent_lname`, `agent_phonenumber`, `agent_email`, `agent_password`) VALUES ('$fname', '$lname','$phonenumber', '$email', '$password')";
 
 
 if (mysqli_query($conn,$sql)) {
