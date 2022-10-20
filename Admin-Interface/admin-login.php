3dd5c44e6e36ef5b01b1aeeadef69083b1a100d1
tkxwaweru <88835284+tkxwaweru@users.noreply.github.com>
2022-10-20 21:31:24 +0300

    Add files via upload

---
+++ Admin-Interface/admin-login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Administrator Login</title>
+		<link rel="stylesheet" href="admin-login.css">
+	</head>
+	<body>
+		<div class="registration-background">
+      <div class="background-image">
+        <img src="../Index/Images/admin.jpg" alt="background image">
+      </div>
+      <div class="registration-form">
+        <div class="form-content">
+          <h2>Login</h2>
+          <form action="..\Processes\admin-login-process.php" method="post"> 
+            <div class="input">
+              <span>Email</span>
+              <input type="text" id="email" name="admin_email" required> 
+            </div>
+            <div class="input">
+              <span>Password</span>
+              <input type="Password" id="password" name="password" required>
+            </div>
+            <div class="remember">
+              <label><input type="checkbox">Remember me</label>
+            </div>
+            <div class="input">
+              <button type="submit" name="login" class="register-button">Log in</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>  
+	</body>
+</html>
---
+++ Admin-Interface/old-interfaces/editagent.php
@@ -0,0 +1,66 @@
+<?php 
+
+$agent_id = $_POST['agent_id']; 
+
+ 
+ 
+
+require("../Processes/DBCONNECT.php"); 
+
+$sql = "SELECT * FROM agents where agent_id='$agent_id'"; 
+
+$result = mysqli_query($conn, $sql); 
+
+ 
+ 
+
+        if (mysqli_num_rows($result) === 1) { 
+
+            $data = mysqli_fetch_assoc($result); 
+
+            echo "<div class='wrapper'> 
+
+        <form action='editagentprocess.php' method='POST'> 
+
+        <input type='hidden' name='hiddenagent_id' value=".$agent_id." /> 
+
+        <p>agent_id:</p> 
+
+        <input type='text' name='agent_id' value=".$data['agent_id']." /> 
+
+        <p>First Name:</p> 
+
+        <input type='text' name='agent_fname' value=".$data['agent_fname']." /> 
+
+        <p>Last Name:</p> 
+
+        <input type='text' name='agent_lname' value=".$data['agent_lname']." /> 
+
+        <p>Phonenumber:</p> 
+
+        <input type='text' name='agent_phonenumber' value=".$data['agent_phonenumber']." /> 
+
+        <p>Email:</p> 
+
+        <input type='text' name='agent_email' value=".$data['agent_email']." /> 
+
+        <p>Password:</p> 
+
+        <input type='text' name='agent_password' value=".$data['agent_password']." /> 
+
+
+        <button type='submit' class='register-button'>Save</button> 
+
+         
+
+        </form> 
+
+        </div>"; 
+
+ 
+
+} 
+
+ 
+
+?> 
