fe9bcb7f6df3842503db8faaad6664bc6525941c
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-10-24 19:53:28 +0300

    Update booking-history.php

--- Client-Interfaces/booking-history.php
+++ Client-Interfaces/booking-history.php
@@ -53,18 +53,34 @@
             <th>Total cost</th>
             <th>Agent code</th>
           </tr>
+
+          <?php
+          require('../Processes/DBCONNECT.php');
+          $inputclientcode = $_POST['client_code'];
+
+          $sql = "SELECT bookings.client_code, bookings.origin, bookings.destination, bookings.departure_date, bookings.return_date, bookings.flight_airline, bookings.accomodation, bookings.transportation, bookings.total_cost, bookings.agent_code FROM bookings WHERE bookings.client_code = '$inputclientcode'";
+          $result = mysqli_query($conn, $sql);
+          $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
+          foreach($test as $key => $value){
+          ?>
+
           <tr>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
-            <td>#</td>
+            <td><?php echo $value['client_code']; ?></td>
+            <td><?php echo $value['origin']; ?></td>
+            <td><?php echo $value['destination']; ?></td>
+            <td><?php echo $value['departure_date']; ?></td>
+            <td><?php echo $value['return_date']; ?></td>
+            <td><?php echo $value['flight_airline']; ?></td>
+            <td><?php echo $value['accomodation']; ?></td>
+            <td><?php echo $value['transportation']; ?></td>
+            <td><?php echo $value['total_cost']; ?></td>
+            <td><?php echo $value['agent_code']; ?></td>
           </tr>
+
+          <?php
+          }
+          ?>
+          
         </table>
 
       </div>
