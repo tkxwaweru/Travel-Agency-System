43c0dd2efeb1fcadff1eedbc2129a9fdb333c5c7
Ikirema <88835650+Ikirema@users.noreply.github.com>
2022-10-24 22:43:21 +0300

    Add files via upload

---
+++ Processes/share-experiences-process.php
@@ -0,0 +1,25 @@
+<?php
+    require('DBCONNECT.php');
+
+        $image = $_POST['image'];
+        $date = $_POST['date'];
+        $location = $_POST['location'];
+        $comment = $_POST['comment'];
+        
+
+        if(empty($date)||empty($location)||empty($comment)){
+            echo "Kindly input details!";
+        }else{
+            $sql = "INSERT INTO experiences (image, date, location, comment) VALUES ('$image','$date','$location','$comment')";
+
+        if(mysqli_query($conn, $sql)){
+             echo "Experience captured successfully";
+           
+        }else{
+            echo "Error: Experience not captured".mysqli_error($conn);
+        }
+
+        }
+
+
+?>
