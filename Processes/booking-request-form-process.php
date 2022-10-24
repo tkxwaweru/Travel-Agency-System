d914d7bbeaa87d6bc4203d07e5aa4939eba6b073
allymoe <112823814+allymoe@users.noreply.github.com>
2022-10-24 16:31:23 +0300

    Update booking-request-form-process.php

--- Processes/booking-request-form-process.php
+++ Processes/booking-request-form-process.php
@@ -1,28 +1,20 @@
 <?php 
 require("DBCONNECT.php");
 
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$phone_number = $_POST['phone_number'];
-$email = $_POST['client_email'];
-$id_number = $_POST['id_number'];
-$flight_type = $_POST['flight_type'];
+$client_code = $_POST['client_code'];
+$origin = $_POST['origin'];
 $destination = $_POST['destination'];
-$flight_class = $_POST['flight_class'];
-$adults = $_POST['adults'];
-$children = $_POST['children'];
-$infants = $_POST['infants'];
-$depart_date = $_POST['depart_date'];
+$traveller_information = $_POST['traveller_information'];
+$preferred_flight_class = $_POST['preferred_flight_class'];
+$departure_date = $_POST['departure_date'];
 $return_date = $_POST['return_date'];
-$accommodation_type = $_POST['accommodation_type'];
-$room_type = $_POST['room_type'];
-$food_type = $_POST['food_type'];
-$transportation_type = $_POST['transportation_type'];
-$bed_type = $_POST['bed_type'];
-$hotel_type = $_POST['hotel_type'];
-$description = $_POST['description'];
+$book_accomodation = $_POST['book_accomodation'];
+$accomodation_description = $_POST['accomodation_description'];
+$book_transportation = $_POST['book_transportation'];
+$transportation_description = $_POST['transportation_description'];
+$agent_code = $_POST['agent_code'];
 
-$sql= "INSERT INTO `booking request`(`fname`, `lname`, `phone_number`, `email`, `id_number`, `fligt_type`, `destination`, `flight_class`, `adults`, `children`, `infants`, `depart_date`, `return_date`, `accommodation_type`, `room_type`, `food_type`, `transportation_type`, `bed_type`, `hotel_type`, `decription`) VALUES ('$fname', '$lname', '$phone_number', '$email', '$id_number', '$flight_type', '$destination', '$flight_class', '$adults', '$children', '$infants', '$depart_date', '$return_date', '$accommodation_type', '$room_type', '$food_type', '$transportation_type', '$bed_type', '$hotel_type', '$description')";
+$sql= "INSERT INTO booking_requests (`client_code`, `origin`, `destination`, `traveller_information`, `preferred_flight_class`, `departure_date`, `return_date`, `book_accomodation`, `accomodation_description`, `book_transportation`, `transportation_description`, `agent_code`) VALUES ('$client_code', '$origin', '$destination', '$traveller_information', '$preferred_flight_class', '$departure_date', '$return_date', '$book_accomodation', '$accomodation_description', '$book_transportation', '$transportation_description', '$agent_code')";
 
 if (mysqli_query($conn,$sql)) {
  echo "Your booking request info has been captured successfully";
