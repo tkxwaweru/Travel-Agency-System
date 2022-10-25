66b944a1a3259d6eae742ae9a49468dc599ece44
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-10-25 13:12:53 +0300

    Update make-booking.php

--- Agent-Interfaces/make-booking.php
+++ Agent-Interfaces/make-booking.php
@@ -34,6 +34,7 @@
         <a href="booking-history-prompt.php">Booking history</a>
         <a href="itinerary.php">Itinerary</a>
         <a  href="agent-invoice.php">Invoice</a>
+        <a  href="payments-prompt.php">Payments</a>
         <a class="log-out-button" href="../Index/index.php">Log out</a>
       </div>
     </nav>
