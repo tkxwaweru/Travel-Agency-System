fb2c93e9ff20c9d8e959c26d877413f9bb4647c0
myles4321 <88835357+myles4321@users.noreply.github.com>
2022-10-25 10:31:52 +0300

    Add files via upload

---
+++ Processes/itinerary-process.php
@@ -0,0 +1,25 @@
+<?php
+
+    require('DBCONNECT.php');
+
+    $client_code = $_POST['client_code'];
+    $e_ticket = $_POST['e_ticket'];
+    $acc_voucher = $_POST['accomodation_voucher'];
+    $transport_voucher = $_POST['transport_voucher'];
+    $agent_code = $_POST['agent_code'];
+
+    $sql = "INSERT INTO itineraries(client_code, e_ticket, accomodation_voucher, transport_voucher, agent_code)
+            VALUES('$client_code', '$e_ticket', '$acc_voucher', '$transport_voucher', '$agent_code')";
+
+
+    if (mysqli_query($conn,$sql)) {
+
+    header("location: ../Agent-Interfaces/itinerary.php ?$$ SUCCESSFUL");
+   
+   }else{echo "Error: Record not added".mysqli_error($conn);}
+
+
+
+
+
+?>
