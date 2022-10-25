6fcfee9e6f771b86cfcf9709e9cc5f86702c8774
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-10-25 13:10:51 +0300

    Add files via upload

---
+++ Agent-Interfaces/payments-prompt.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Payments</title>
+  <link rel="stylesheet" href="agentHome.css" />
+  <link rel="stylesheet" href="clients-prompt.css">
+  <!-- Font Awesome Cdn Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+  <header class="header">
+    <div class="title">
+      <span>Dashboard</span>
+    </div>
+    <div class="header-icons">
+      <div class="account">
+        <h4><h4><?php  include("../Processes/agent-login-process.php"); ?><?php  echo $_SESSION['agent_email']; ?></h4></h4>
+      </div>
+    </div>
+  </header>
+
+  <div class="container">
+    <nav>
+    
+      <div class="side_navbar">
+        <a href="agentHome.php">Home</a>
+        <a href="clients-prompt.php">Clients</a>
+        <a href="make-booking.php">Make Booking</a>
+        <a href="booking-requests-prompt.php">Booking Requests</a>
+        <a href="booking-history-prompt.php">Booking history</a>
+        <a href="itinerary.php">Itinerary</a>
+        <a  href="invoice.php">Invoice</a>
+        <a  href="view-invoices-prompt.php">View Invoices</a>
+        <a  class="active" href="payments-prompt.php">Payments</a>
+        <a class="log-out-button" href="../Index/index.php">Log out</a>
+      </div>
+    </nav>
+
+    <div class="main-body">
+      <div class="form-box">
+        <form action="payments.php" method="POST">
+            <h3>Enter Client Code</h3><br>
+            <input type="text" name="client_code" id="input-text"><br>
+            <input type="submit" value="Submit" class="button"> <br><br>
+        </form>
+      </div>
+
+
+    </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ Agent-Interfaces/payments.php
@@ -0,0 +1,123 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Payments</title>
+  <link rel="stylesheet" href="agentHome.css" />
+  <link rel="stylesheet" href="clients.css">
+  <!-- Font Awesome Cdn Link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+</head>
+<body>
+  <header class="header">
+    <div class="title">
+      <span>Dashboard</span>
+    </div>
+    <div class="header-icons">
+      <div class="account">
+        <h4><h4><?php  include("../Processes/agent-login-process.php"); ?><?php  echo $_SESSION['agent_email']; ?></h4></h4>
+      </div>
+    </div>
+  </header>
+
+  <div class="container">
+    <nav>
+    
+      <div class="side_navbar">
+        <a href="agentHome.php">Home</a>
+        <a href="clients-prompt.php">Clients</a>
+        <a href="make-booking.php">Make Booking</a>
+        <a href="booking-requests-prompt.php">Booking Requests</a>
+        <a href="booking-history-prompt.php">Booking history</a>
+        <a href="itinerary.php">Itinerary</a>
+        <a  href="invoice.php">Invoice</a>
+        <a  href="view-invoices-prompt.php">View Invoices</a>
+        <a  class="active"  href="payments-prompt.php">Payments</a>
+        <a class="log-out-button" href="../Index/index.php">Log out</a>
+      </div>
+    </nav>
+
+    
+    <div class="main-body">
+      <h2>M-Pesa Payments</h2><br><br>
+        <div class="table">
+            <table>
+                <tr>
+                    <th>Payment ID</th>
+                    <th>Client Code</th>
+                    <th>Confirmation</th>
+                    <th>Accomodation Code</th>
+                    <th>Payment Date</th>
+                    <th>Issued</th>
+                </tr>
+
+                <?php
+                  require('../Processes/DBCONNECT.php');
+                  $client_code = $_POST['client_code'];
+
+                  $sql = "SELECT * FROM mpesa_payments WHERE mpesa_payments.client_code = '$client_code' AND mpesa_payments.issued = '0'";
+                  $result = mysqli_query($conn, $sql);
+                  $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                  foreach($test as $key => $value){
+                  
+
+                ?>
+                <tr>
+                    <td><?php echo $value['payment_id'] ?></td>
+                    <td><?php echo $value['client_code'] ?></td>
+                    <td><?php echo $value['confirmation_code'] ?></td>
+                    <td><?php echo $value['payment_date'] ?></td>
+                    <td><?php echo $value['issued'] ?></td>
+                </tr>
+                <?php
+                  }
+                ?>
+            </table>
+
+            <br><br><br>
+
+            <h2>Card Payments</h2><br><br>
+        <div class="table">
+            <table>
+                <tr>
+                    <th>Client Code</th>
+                    <th>Agent Code</th>
+                    <th>Flight Total</th>
+                    <th>Accomodation Total</th>
+                    <th>Transportation Total</th>
+                    <th>Service Total</th>
+                    <th>Service Charge</th>
+                    <th>Subtotal</th>
+                </tr>
+
+                <?php
+                  require('../Processes/DBCONNECT.php');
+                  $client_code = $_POST['client_code'];
+
+                  $sql = "SELECT card_payments.payment_id, card_payments.client_code, card_payments.card_owner, card_payments.payment_date, card_payments.issued FROM card_payments WHERE card_payments.client_code = '$client_code' AND card_payments.issued = '0'";
+                  $result = mysqli_query($conn, $sql);
+                  $test = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                  foreach($test as $key => $value){
+                  
+
+                ?>
+                <tr>
+                    <td><?php echo $value['payment_id'] ?></td>
+                    <td><?php echo $value['client_code'] ?></td>
+                    <td><?php echo $value['card_owner'] ?></td>
+                    <td><?php echo $value['payment_date'] ?></td>
+                    <td><?php echo $value['issued'] ?></td>
+                </tr>
+                <?php
+                  }
+                ?>
+            </table>
+        </div>
+
+
+    </div>
+    </div>
+  </div>
+</body>
+</html>
