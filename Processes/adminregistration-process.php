10337d1f39c19857c3b8400d79db4b31d5a24698
quantumfelonies <38161851+quantumfelonies@users.noreply.github.com>
2022-10-25 09:47:16 +0300

    Add files via upload

---
+++ Processes/adminregistration-process.php
@@ -0,0 +1,17 @@
+<?php 
+require("DBCONNECT.php");
+
+$admin_name = $_POST['admin_name'];
+$admin_email = $_POST['admin_email'];
+$admin_password = $_POST['admin_password'];
+
+
+$sql= "INSERT INTO `admin`(`admin_name`, `admin_email`, `admin_password`) 
+VALUES ('$admin_name', '$admin_email', '$admin_password')";
+
+
+if (mysqli_query($conn,$sql)) {
+ echo "Agent added Successful.";
+}else{echo "Error: Record not added".mysqli_error($conn);}
+
+?>
