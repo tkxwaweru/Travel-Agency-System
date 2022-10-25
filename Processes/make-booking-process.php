85d5e1520757458b074ab328a9ca15a7b2631960
myles4321 <88835357+myles4321@users.noreply.github.com>
2022-10-25 10:15:54 +0300

    Add files via upload

---
+++ Processes/make-booking-process.php
@@ -0,0 +1,29 @@
+<?php
+
+    require('DBCONNECT.php');
+
+    $client_code = $_POST['client_code'];
+    $origin = $_POST['origin'];
+    $destination = $_POST['destination'];
+    $depart_date = $_POST['dDate'];
+    $return_date = $_POST['rDate'];
+    $airline = $_POST['airline'];
+    $transport = $_POST['transport'];
+    $agent_code = $_POST['agent_code'];
+    $total_cost = $_POST['total_cost'];
+
+    $sql = "INSERT INTO bookings(client_code, origin, destination, departure_date, return_date, flight_airline, transportation, total_cost, agent_code)
+            VALUES('$client_code', '$origin', '$destination', '$depart_date', '$return_date', '$airline', '$transport', '$total_cost', '$agent_code')";
+
+
+    if (mysqli_query($conn,$sql)) {
+
+    header("location: ../Agent-Interfaces/make-booking.php");
+   
+   }else{echo "Error: Record not added".mysqli_error($conn);}
+
+
+
+
+
+?>
