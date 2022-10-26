e09b20e4eb8f2af4b1d490401ef2a2bf624db727
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-10-26 10:16:43 +0300

    Update booking-requests.php

--- Agent-Interfaces/booking-requests.php
+++ Agent-Interfaces/booking-requests.php
@@ -32,6 +32,7 @@
         <a href="booking-history-prompt.php">Booking history</a>
         <a href="itinerary.php">Itinerary</a>
         <a  href="invoice.php">Invoice</a>
+        <a  href="view-invoice-prompt.php">View Invoices</a>
         <a  href="payments-prompt.php">Payments</a>
         <a class="log-out-button" href="../Index/index.php">Log out</a>
       </div>
