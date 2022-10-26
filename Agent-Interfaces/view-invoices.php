076363e99b4bdac602953f03fa3b2fe4e6f97fa3
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-10-26 10:21:12 +0300

    Update view-invoices.php

--- Agent-Interfaces/view-invoices.php
+++ Agent-Interfaces/view-invoices.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8" />
-  <title>Clients</title>
+  <title>Invoices</title>
   <link rel="stylesheet" href="agentHome.css" />
   <link rel="stylesheet" href="clients.css">
   <!-- Font Awesome Cdn Link -->
