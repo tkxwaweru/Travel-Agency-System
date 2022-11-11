c2736c8303f6116513ba43844bdf881a45912e11
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-11-11 12:14:32 +0300

    Update booking-requests-prompt.php

--- Agent-Interfaces/booking-requests-prompt.php
+++ Agent-Interfaces/booking-requests-prompt.php
@@ -42,7 +42,7 @@
       <div class="form-box">
         <form action="booking-requests.php" method="post">
             <h3>Enter Your Agent Code</h3><br>
-            <input type="text" name="agent_code" id="input-text"><br>
+            <input type="text" name="agent_code" id="input-text" required><br>
             <input type="submit" value="Submit" class="button"> <br><br>
         </form>
       </div>
