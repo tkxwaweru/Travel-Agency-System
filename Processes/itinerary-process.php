2300789dc3b57a7029f39ef818311404e39ee308
tkxwaweru <88835284+tkxwaweru@users.noreply.github.com>
2022-11-12 19:59:16 +0300

    Update itinerary-process.php

--- Processes/itinerary-process.php
+++ Processes/itinerary-process.php
@@ -3,13 +3,20 @@
     require('DBCONNECT.php');
 
     $client_code = $_POST['client_code'];
-    $e_ticket = $_POST['e_ticket'];
-    $acc_voucher = $_POST['accomodation_voucher'];
-    $transport_voucher = $_POST['transport_voucher'];
+    $e_ticket = $_FILES['e_ticket'];
+    $acc_voucher = $_FILES['accomodation_voucher'];
+    $transport_voucher = $_FILES['transport_voucher'];
     $agent_code = $_POST['agent_code'];
 
+
+    
+    $blob1 = addslashes(file_get_contents($e_ticket["tmp_name"]));
+    $blob2 = addslashes(file_get_contents($acc_voucher["tmp_name"]));
+    $blob3 = addslashes(file_get_contents($transport_voucher["tmp_name"]));
+    
+
     $sql = "INSERT INTO itineraries(client_code, e_ticket, accomodation_voucher, transport_voucher, agent_code)
-            VALUES('$client_code', '$e_ticket', '$acc_voucher', '$transport_voucher', '$agent_code')";
+            VALUES('$client_code', '$blob1', '$blob2', '$blob3', '$agent_code')";
 
 
     if (mysqli_query($conn,$sql)) {
@@ -17,9 +24,3 @@
     header("location: ../Agent-Interfaces/itinerary-popup.php");
    
    }else{echo "Error: Record not added".mysqli_error($conn);}
-
-
-
-
-
-?>
