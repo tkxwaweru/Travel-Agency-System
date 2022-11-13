cf7065f5fffc453edcceb6095ec3dd204cd5dd96
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-11-13 16:46:34 +0300

    Update invoice-process.php

--- Processes/invoice-process.php
+++ Processes/invoice-process.php
@@ -5,8 +5,11 @@
     $client_code = $_POST['client-code'];
     $agent_code = $_POST['agent-code'];
     $flight_total = $_POST['flight-total'];
+    $flight_total=   $flight_total*120;
     $accomodation_total = $_POST['accomodation-total'];
+    $accomodation_total=$accomodation_total*120;
     $transport_total = $_POST['transport-total'];
+    $transport_total=$transport_total*120;
     $service_total = $flight_total + $accomodation_total + $transport_total;
     $service_charge = $service_total * 0.1;
     $subtotal = $service_total + $service_charge;
