3e94e2780bb8d1f0c3bddaca65733a2a9d4d2d07
FarajaOmbeni <88835733+FarajaOmbeni@users.noreply.github.com>
2022-11-13 16:26:24 +0300

    Update make-booking.php

--- Agent-Interfaces/make-booking.php
+++ Agent-Interfaces/make-booking.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8" />
   <link rel="stylesheet" href="make-booking.css">
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   
   <title>Make Booking</title>
 </head>
